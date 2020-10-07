da20398961e976ed7939a2955dc57322967d62b6
BHessel <hesselfitness@gmail.com>
2020-10-07 16:09:52 -0400

    update through clean/happy

--- lib/person.rb
+++ lib/person.rb
@@ -2,24 +2,56 @@
 require 'pry'
 class Person 
 
-def initialize(name)
-   @name = name
-   @bank_account = 25
-end
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
 
-def name
-  @name
-end
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
 
-def bank_account
-  @bank_account
-end
+  def happiness
+    if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
+    else
+      @happiness
+    end
+  end
+
+  def hygiene
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    else
+      @hygiene
+    end
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    if @hygiene > 7
+      return true
+    else
+      return false
+    end
+  end
+#why weren't we able to do the same code for clean? and happy?
+
+
 
 end
 stella = Person.new("Stella")
 stella.name
 
 blake = Person.new("Blake") 
-blake.bank_account
-
+blake.bank_account #= 30
+#binding.pry
 
